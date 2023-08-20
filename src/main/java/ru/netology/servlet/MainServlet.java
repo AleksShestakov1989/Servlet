787dce4ea6a619a06453ee27d45f5e4af9918aa4
Aleks <shestakov.aleksey8989@gmail.com>
2023-08-20 22:10:02 +0300

    Add feature/di-java

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,73 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private static final String API_POSTS = "/api/posts";
+    private static final String API_POSTS_ID = "/api/posts/\\d+";
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+
+
+    //    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws  IOException{
+//        resp.setStatus(HttpServletResponse.SC_CONFLICT);
+//        resp.setContentType("text/plain");
+//        resp.setHeader("MyCustomHeader","MyCustomHeaderValue");
+//        resp.getWriter().print("Hello from main servlet");
+//    }
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(GET) && path.equals(API_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches(API_POSTS_ID)) {
+                // easy way
+                controller.getById(idGetPost(path), resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(API_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches(API_POSTS_ID)) {
+                // easy way
+                controller.removeById(idGetPost(path), resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private long idGetPost(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+}
